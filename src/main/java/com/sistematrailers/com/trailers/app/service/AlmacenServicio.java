15d2ee7f47de26ad3d759a852a85fb46e6835503
Leandro <leandrostev@hotmail.com>
2023-03-02 20:34:28 -0300

    Metodos implementados en service.

--- src/main/java/com/sistematrailers/com/trailers/app/service/AlmacenServicio.java
+++ src/main/java/com/sistematrailers/com/trailers/app/service/AlmacenServicio.java
@@ -1,13 +1,13 @@
 package com.sistematrailers.com.trailers.app.service;
 
-import jakarta.annotation.Resource;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Path;
 
 
-public interface AlmacenServicio {
+public interface  AlmacenServicio {
 
     public void iniciarAlmacenDeArchivos();
     public String almacenarArchivo(MultipartFile archivo);
@@ -16,6 +16,6 @@ public interface AlmacenServicio {
 
     public Resource cargarComoRecurso(String nombreArchivo);
 
-    public void eliminarArchivo(String eliminarArchivo);
+    public void eliminarArchivo(String nombreArchivo);
 
 }
--- src/main/java/com/sistematrailers/com/trailers/app/service/AlmacenServicioImpl.java
+++ src/main/java/com/sistematrailers/com/trailers/app/service/AlmacenServicioImpl.java
@@ -1,16 +1,22 @@
 package com.sistematrailers.com.trailers.app.service;
 
 import com.sistematrailers.com.trailers.app.exceptions.AlmacenException;
+import com.sistematrailers.com.trailers.app.exceptions.FileNotFoundException;
 import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class AlmacenServicioImpl implements AlmacenServicio{
@@ -36,21 +42,49 @@ public class AlmacenServicioImpl implements AlmacenServicio{
 
     @Override
     public String almacenarArchivo(MultipartFile archivo) {
-        return null;
+        String nombreArchivo = archivo.getOriginalFilename();
+        if (archivo.isEmpty()){
+            throw new AlmacenException("No se puede almacenar un archivo vacio");
+        }
+            try {
+                InputStream inputStream= archivo.getInputStream();
+                Files.copy(inputStream,Paths.get(storageLocation).resolve(nombreArchivo), StandardCopyOption.REPLACE_EXISTING);
+            }catch (IOException exception){
+                throw new AlmacenException("Error al almacenar el archivo" + nombreArchivo,exception);
+            }
+        return nombreArchivo;
     }
+
 
     @Override
     public Path cargarArchivo(String nombreArchivo) {
-        return null;
+      return Paths.get(storageLocation).resolve(nombreArchivo);
     }
 
     @Override
     public Resource cargarComoRecurso(String nombreArchivo) {
-        return null;
+        try {
+                Path archivo = cargarArchivo(nombreArchivo);
+                Resource recurso = new UrlResource(archivo.toUri());
+                if (recurso.exists() || recurso.isReadable()){
+                    return recurso;
+
+                }else {
+                    throw new FileNotFoundException("No se pudo encontrar el archivo" + nombreArchivo);
+                }
+        }catch (MalformedURLException exception){
+                throw new FileNotFoundException("No se pudo encontrar el archivo" + nombreArchivo,exception);
+
+        }
     }
 
     @Override
-    public void eliminarArchivo(String eliminarArchivo) {
-
-    }
+    public void eliminarArchivo(String nombreArchivo) {
+        Path archivo = cargarArchivo(nombreArchivo);
+        try {
+            FileSystemUtils.deleteRecursively(archivo);
+        }catch (Exception exception) {
+            System.out.println(exception);
+        }
+        }
 }
