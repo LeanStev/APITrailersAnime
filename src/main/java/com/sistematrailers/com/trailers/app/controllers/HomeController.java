fb42233d9dd6130186a8c815c001d924aaeca22d
Leandro <leandrostev@hotmail.com>
2023-03-10 21:50:51 -0300

    Toda la logica ya esta hecha. Falta estilizar y listo

--- src/main/java/com/sistematrailers/com/trailers/app/controllers/HomeController.java
+++ src/main/java/com/sistematrailers/com/trailers/app/controllers/HomeController.java
@@ -1,7 +1,8 @@
 package com.sistematrailers.com.trailers.app.controllers;
 
-import com.sistematrailers.com.trailers.app.model.Anime;
-import com.sistematrailers.com.trailers.app.repositorios.AnimeRepositorio;
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,12 +10,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import com.sistematrailers.com.trailers.app.model.Anime;
+import com.sistematrailers.com.trailers.app.repositorios.AnimeRepositorio;
 
 @Controller
 @RequestMapping("")
@@ -35,5 +37,11 @@ public class HomeController {
         Page<Anime> animes = animeRepositorio.findAll(pageable);
         return new ModelAndView("animes")
                 .addObject("animes",animes);
+    }
+
+    @GetMapping("/animes/{id}")
+    public ModelAndView mostrarDetallesDeAnime(@PathVariable Integer id){
+        Anime anime = animeRepositorio.getOne(id);
+        return new ModelAndView("anime").addObject("anime",anime);
     }
 }
